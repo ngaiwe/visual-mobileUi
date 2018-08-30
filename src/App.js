0bccb6cc8a7cd7188bca5f889b51f0dd9b676f7d
weiran <weiran@vipkid.com.cn>
2018-08-30 20:14:26 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import {Button} from 'antd';
 import './css/reset.css'
+import './App.less'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        123456
+        <div className="text">123</div>
+        <Button type="primary">Button</Button>
         {this.props.children}
       </div>
     )
